2c1725e29df22af914e28e0570cb97f0bb42fe39
Varun Bondada <84311117+varunbondada001@users.noreply.github.com>
2024-03-13 21:19:49 +0530

    Update Swap.java

--- Assignment-2/Swap.java
+++ Assignment-2/Swap.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 class Sum {
     public static void main(String[] args)
     {
-        int []values={10,51,53,39,47,44,21};
+        int []values={3,5,739,41,55,66};
         Arrays.sort(values);
         int length=values.length;
         int sum=values[length-2]+values[length-3];
