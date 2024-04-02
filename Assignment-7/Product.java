cabdd854619894e8da608030065e255fec525bff
Varun Bondada <84311117+varunbondada001@users.noreply.github.com>
2024-04-02 10:49:52 +0530

    Create Product.java

---
+++ Assignment-7/Product.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Product {
+    private double price;
+
+    public Product(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
